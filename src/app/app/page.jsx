7944b451361450e499778de3edf9b27ad0d94241
dolodorsey <dolodorsey@gmail.com>
2026-08-09 07:32:27 -0400

    feat: mount complete S.O.S. customer surfaces

--- src/app/app/page.jsx
+++ src/app/app/page.jsx
@@ -3,6 +3,10 @@
 import dynamic from 'next/dynamic';
 import SOSCustomerOperationsHost from '@/components/SOSCustomerOperationsHost';
 import SOSCustomerCancellationHost from '@/components/SOSCustomerCancellationHost';
+import SOSCustomerCoverageStatusHost from '@/components/SOSCustomerCoverageStatusHost';
+import SOSCustomerReceiptHost from '@/components/SOSCustomerReceiptHost';
+import SOSNotificationInboxHost from '@/components/SOSNotificationInboxHost';
+import SOSShareTrackingHost from '@/components/SOSShareTrackingHost';
 import SOSSettlementReviewHost from '@/components/SOSSettlementReviewHost';
 import SOSPaymentReadinessHost from '@/components/SOSPaymentReadinessHost';
 import SOSMissionChatHost from '@/components/SOSMissionChatHost';
@@ -17,5 +21,5 @@ const SOSCustomerRealtimeShell = dynamic(() => import('@/components/SOSCustomerR
 });
 
 export default function AppPage() {
-  return <><SOSPaymentReadinessHost audience="customer"/><SOSRecoveryHost audience="customer"/><SOSPushRegistrationHost/><SOSCustomerRealtimeShell/><SOSCustomerOperationsHost/><SOSCustomerCancellationHost/><SOSSettlementReviewHost/><SOSMissionChatHost/><SOSMembershipHost/><SOSProfileToolsHost/></>;
+  return <><SOSPaymentReadinessHost audience="customer"/><SOSRecoveryHost audience="customer"/><SOSPushRegistrationHost/><SOSNotificationInboxHost/><SOSCustomerRealtimeShell/><SOSCustomerCoverageStatusHost/><SOSCustomerOperationsHost/><SOSCustomerCancellationHost/><SOSSettlementReviewHost/><SOSMissionChatHost/><SOSMembershipHost/><SOSProfileToolsHost/><SOSCustomerReceiptHost/><SOSShareTrackingHost/></>;
 }
