49811d0cf8959d04802f961a08774d6fdf0b287a
dolodorsey <dolodorsey@gmail.com>
2026-09-17 21:16:13 -0400

    fix: use official S.O.S. logo for Home Screen icons

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -34,7 +34,7 @@ export const metadata = {
       { url: '/favicon.ico', sizes: 'any' },
       { url: '/favicon.png', type: 'image/png', sizes: '32x32' },
     ],
-    apple: '/apple-touch-icon.png',
+    apple: '/app-logo.png',
   },
   openGraph: {
     title: 'S.O.S. — Superheroes On Standby',
